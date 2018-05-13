d26bd6910900d66831bd7b4cdcf6e4f955d91d8c
Daniel <danielochoa@outlook.com>
2018-05-12 20:13:41 -0500

    Interface DaoActivo

---
+++ riesgoDifuso_v2/src/java/dao/DaoActivo.java
@@ -0,0 +1,33 @@
+/*
+ Interface para la clase Activo
+ Acá se visualiza lo que se implementará
+ */
+package dao;
+
+import dto.Activo;
+import java.util.List;
+
+/**
+ *
+ * @author DANIEL
+ */
+public interface DaoActivo {
+
+    public List<Object[]> activoQry(); // Listado de los activos
+
+    public List<Object[]> activoPorNombreQry(String nombact); // Listado de los activos por nombre
+
+    public String activoIns(Activo activo); // Método para insertar (ins) los activos
+
+    public String activoDel(List<Integer> ids); // Método para eliminar (del) a una activo 
+
+    // Método para actualizar (upd)
+    public Activo activoGet(Integer idactivo); // Para buscar un activo
+
+    public String activoUpd(Activo activo); // Actualiza el activo escogido
+
+    public List<Object[]> activoCbo(); // Llenar comboBox
+
+    // Método que devuelve un mensaje de error para el listar
+    public String getMessage();
+}
