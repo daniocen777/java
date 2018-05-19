884c671aba7384c5cc6d9d0bfa0188fc59b55530
Daniel <danielochoa@outlook.com>
2018-05-18 20:20:49 -0500

    JS fomrmulario

--- riesgoDifuso_v2/web/parainfo/form.js
+++ riesgoDifuso_v2/web/parainfo/form.js
@@ -1,7 +1,7 @@
 $(function () {
     $("form.parainfo fieldset").addClass("ui-widget-content");
     $("form.parainfo legend").addClass("ui-widget-header ui-widget-content ui-corner-all");
-
+    
     // input ==> Esquinas redondeadas
     $("form.parainfo input[type=text]").addClass("ui-widget ui-widget-content ui-corner-all");
     $("form.parainfo input[type=password]").addClass("ui-widget ui-widget-content ui-corner-all");
